ac779d58020efc47f321eb3400a89afc9211a407
Tesh Pierre <tesh@Teshs-MacBook-Air.local>
2023-12-31 09:07:18 -0700

    cleaning up issues with add vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-// createApp(App).mount('#app')
+Vue.use(Vuetify)
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const firebaseConfig = {
+    apiKey: "AIzaSyADNVyxnt7968BXtQN0Wm5Arr5E_cRNa_8",
+    authDomain: "personal-website-c86c9.firebaseapp.com",
+    databaseURL: "https://personal-website-c86c9-default-rtdb.firebaseio.com",
+    projectId: "personal-website-c86c9",
+    storageBucket: "personal-website-c86c9.appspot.com",
+    messagingSenderId: "847593821017",
+    appId: "1:847593821017:web:d1483c7452f41edcc43248",
+    measurementId: "G-DDGX5LQ7XX"
+  };
+  
+
+initializeApp(firebaseConfig)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,6 @@ import Home from '@/views/Home.vue';
 import Contact from '@/views/Contact.vue';
 import Projects from '@/views/Projects.vue';
 import AboutMe from '@/views/AboutMe.vue';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getDatabase } from 'firebase/database';
 
 const routes = [
   { path: '/', component: Home },
@@ -21,32 +17,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
-// Initialize firebase app and add project information
-const firebaseApp = initializeApp({
-  apiKey: 'string',
-  authDomain: 'string',
-  projectId: 'string',
-  storageBucket: 'string',
-  messagingSenderId: 'string',
-  appId: 'string',
-  measurementId: 'string'
-});
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://personal-website-c86c9-default-rtdb.firebaseio.com",
-
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
 
 export default router;
